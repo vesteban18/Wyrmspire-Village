7f61f8b6cd307752256f3ba6abccefc42f44f79c
jared-baker99 <j.baker.99@knights.ucf.edu>
2023-10-07 15:37:27 -0400

    Update TimeController.cs

--- Wyrmspire-Village/Assets/TimeController.cs
+++ Wyrmspire-Village/Assets/TimeController.cs
@@ -15,7 +15,14 @@ public class TimeController : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI timeText;
 
+    [SerializeField]
+    private TextMeshProUGUI dayText;
+
     private DateTime currentTime;
+
+    public DateTime dayTime;
+    private bool flag;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -32,9 +39,22 @@ public class TimeController : MonoBehaviour
     {
         currentTime = currentTime.AddSeconds(Time.deltaTime * timeMultiplier);
 
-        if(timeText != null)
+        if (timeText != null)
         {
             timeText.text = currentTime.ToString("HH:mm");
+            if (timeText.text == "00:00")
+            {
+                if (flag == false)
+                {
+                    dayTime = dayTime.AddDays(1);
+                    dayText.text = dayTime.ToString("dd");
+                    flag = true;
+                }
+            }
+            if (timeText.text == "12:00")
+            {
+                flag = false;
+            }
         }
     }
 }
