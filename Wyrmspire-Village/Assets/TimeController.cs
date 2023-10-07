33503767b5e0d6684c39e70cb0d9c4ef20f0a006
jared-baker99 <j.baker.99@knights.ucf.edu>
2023-10-07 10:18:10 -0400

    Got time clock running

--- Wyrmspire-Village/Assets/TimeController.cs
+++ Wyrmspire-Village/Assets/TimeController.cs
@@ -1,18 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TimeController : MonoBehaviour
 {
+    [SerializeField]
+    private float timeMultiplier;
+
+    [SerializeField]
+    private float startHour;
+
+    [SerializeField]
+    private TextMeshProUGUI timeText;
+
+    private DateTime currentTime;
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentTime = DateTime.Now.Date + TimeSpan.FromHours(startHour);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateTimeOfDay();
+    }
+
+    private void UpdateTimeOfDay()
+    {
+        currentTime = currentTime.AddSeconds(Time.deltaTime * timeMultiplier);
+
+        if(timeText != null)
+        {
+            timeText.text = currentTime.ToString("HH:mm");
+        }
     }
 }
