dfe04feb1fe351b7e7059a654271574742592977
NoahLawton <120597531+NoahLawton@users.noreply.github.com>
2023-10-07 23:06:37 -0400

    Villager collision detection
    Mostly working

--- Wyrmspire-Village/Assets/Villager.cs
+++ Wyrmspire-Village/Assets/Villager.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 public class Villager : MonoBehaviour
 {
     private TimeController timeController;
-    private static bool isFemale;
+    private bool isFemale;
     private float hunger;
     private float happiness;
     private float moveSpeed = 30.0f;
     private bool pregnant;
     private bool eatFlag = false;
     private bool pregnantFlag = false;
-    private Vector3 targetPosition;
+    private Vector2 targetPosition;
     private int localDay;
     private float timeSinceDirectionChange;
     private float changeDirectionInterval;
+    private float maxX = 500;
+    private float minX = -500;
+    private float maxY = 500;
+    private float minY = -500;
+
     // Start is called before the first frame update
 
 /*
@@ -48,22 +53,39 @@ public class Villager : MonoBehaviour
         timeSinceDirectionChange = 0.0f;
         changeDirectionInterval = 0.5f;
 
-        targetPosition = transform.position;
         localDay = -1;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Villager is out of range
+        if (transform.position.x > maxX || transform.position.x < minX || transform.position.y > maxY || transform.position.y < minY)
+        {
+            transform.position = new Vector2(0, 0);
+        }
         timeSinceDirectionChange += Time.deltaTime;
         if (timeSinceDirectionChange >= changeDirectionInterval)
         {
-            float randomX = Random.Range(-500.0f, 500.0f);
-            float randomY = Random.Range(-500.0f, 500.0f);
-            targetPosition = new Vector2(randomX, randomY);
+            //Vector2 newTargetPosition = GetPositionWithoutObstacle();
+            targetPosition = GetPositionWithoutObstacle();
+            //targetPosition = new Vector2(newTargetPosition.x, newTargetPosition.y);
             timeSinceDirectionChange = 0.0f;
         }
+
+        Vector2 moveDirection = (targetPosition - (Vector2)transform.position).normalized;
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, moveDirection, 1.0f);
+
+        if (hit.collider != null)
+        {
+            Vector2 newRandomDirection = Random.insideUnitCircle.normalized;
+            targetPosition = new Vector2(transform.position.x + newRandomDirection.x, transform.position.y + newRandomDirection.y);
+        }
+
         transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+
+
 
         // Daily events
     if (localDay != timeController.Day)
@@ -96,5 +118,49 @@ public class Villager : MonoBehaviour
         }
     }
     eatFlag = false;
+    }
+
+    Vector2 GetPositionWithoutObstacle()
+    {
+        Vector2 randomPosition = Vector2.zero;
+        bool obstacleDetected = true;
+        int maxAttempts = 10;
+        int attempts = 0;
+
+        while(obstacleDetected && attempts < maxAttempts)
+        {
+            attempts++;
+
+            float randomX = Random.Range(-750.0f, 750.0f);
+            float randomY = Random.Range(-750.0f, 750.0f);
+
+            targetPosition = new Vector2(randomX, randomY);
+
+            Collider2D[] hitColliders = Physics2D.OverlapCircleAll(randomPosition, 10.0f);
+
+            bool obstacleFound = false;
+
+            foreach (Collider2D collider in hitColliders)
+            {
+                if (collider.gameObject.CompareTag("Grid"))
+                {
+                    obstacleFound = true;
+                    break;
+                }
+            }
+
+            if (!obstacleFound)
+            {
+                obstacleDetected = false;
+            }
+
+            RaycastHit2D hit = Physics2D.Raycast(targetPosition, Vector2.zero, 1.0f);
+
+            if (hit.collider == null)
+            {
+                obstacleDetected = false;
+            }
+        }
+        return targetPosition;
     }
 }
